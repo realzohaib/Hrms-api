064269533e532cf6f00f7b9d991cd8b931690499
MohammadFurqan095 <furqank095@gmail.com>
2023-10-19 17:37:40 +0530

    extra benefits data save to database and file save to folder

--- src/main/java/com/erp/hrms/form/service/IExtraBenefitsApprovalService.java
+++ src/main/java/com/erp/hrms/form/service/IExtraBenefitsApprovalService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface IExtraBenefitsApprovalService {
 
-	
-	public void createExtraBenefitsApproval(String extraBenefitsApproval, String supportingDocuments) throws IOException;
-	
+	public void createExtraBenefitsApproval(String extraBenefitsApproval, MultipartFile supportingDocumentsName)
+			throws IOException;
+
 }
