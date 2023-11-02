2d30ada527febd915a821caeebcd567b73ea6e0e
realzohaib <itszohaib29@gmail.com>
2023-11-02 15:06:38 +0530

    deleted forms module

--- src/main/java/com/erp/hrms/form/repository/IExtraBenefitsApprovalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.erp.hrms.form.repository;
-
-import com.erp.hrms.entity.form.ExtraBenefitsApproval;
-
-public interface IExtraBenefitsApprovalRepository {
-
-	public void createExtraBenefitsApproval(ExtraBenefitsApproval extraBenefitesApproval);
-}
